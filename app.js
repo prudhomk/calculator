5807bd3f774058533c7a8c74de8974d536810d0c
Kalan Prudhomme <cleaver9@gmail.com>
2021-02-10 15:43:23 -0800

    start of build

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 // import functions and grab DOM elements
+const firstInput = document.getElementById('first-input');
+const secondInput = document.getElementById('second-input');
+const changeButton = document.getElementById('calculate');
+const solutionDisplay = document.getElementById('solution');
+
 
 // initialize state
-
+changeButton.addEventListener('compute', () => {
+	valueDisplay.content = solution.value;
+});
 // set event listeners to update state and DOM
+console.log('hello');
