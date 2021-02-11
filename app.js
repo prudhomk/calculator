0b1150e0a4fcd76e3d517fe6899938691a20f52f
Kalan Prudhomme <cleaver9@gmail.com>
2021-02-10 17:02:46 -0800

    even cleaner

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 // grab DOM elements
-const x = document.getElementById('first-input');
-const y = document.getElementById('second-input');
+const value1 = document.getElementById('x');
+const value2 = document.getElementById('y');
 const solutionDisplay = document.getElementById('solution');
-
-const calculator = document.querySelector('.calculator');
-const keys = calculator.querySelector('.calculator__keys')
 
 // set event listeners to update state and DOM
 
-changeButton.addEventListener('click', () => {
+add.addEventListener('click', () => {
 	const solution = x.value + y.value;
 	solutionDisplay.text = sum;
 
 });
+
+let sum = (x.value, y.value) => }
+	return x + y;
+};
+
+add.addEventListerner('click', () => {
+	typeof(Number(value1.value))  solution || typeof(Number(vaule2.value)) === solution) {
+
+})
+
+solutionDisplay.value = 
